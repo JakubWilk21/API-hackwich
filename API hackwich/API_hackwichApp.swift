2180560970b2da07c8ee5920e0d1b9d2c283d26c
Jakub <Wilk>
2023-02-06 14:43:33 -0600

    Initial Commit

---
+++ API hackwich/API_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_hackwichApp.swift
+//  API hackwich
+//
+//  Created by Jakub Wilk on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct API_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
