66e258e937c3940acefe04b12ec32389fe28b56e
Jakub <Wilk>
2023-02-06 20:01:02 -0600

    Set up API query

--- API hackwich/ContentView.swift
+++ API hackwich/ContentView.swift
@@ -24,13 +24,29 @@ struct ContentView: View {
         }
     }
     func getJokes() {
-        let apiKey = "?rapidapi-key=1a521986b0msh0ecfd357b9123fbp1ca6cfjsnb7ef71545a5d
+        let apiKey = "?rapidapi-key=1a521986b0msh0ecfd357b9123fbp1ca6cfjsnb7ef71545a5d"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let setup = item["setup"].stringValue
+                        let punchline = item["punchline"].stringValue
+                        let joke = Joke(setup: setup, punchline: punchline)
+                        jokes.append(joke)
+                    }
+                }
+            }
+            
         }
+    }
+    struct Joke: Identifiable {
+        let id = UUID()
+        var setup = ""
+        var punchline = ""
+    }
+    
+    
 }
-        struct Joke: Identifiable {
-            let id = UUID()
-            var setup = ""
-            var punchline = ""
-        }
-
- 
