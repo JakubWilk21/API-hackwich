3ccc43ec643322ed944985389b2c45f9c41b45db
Jakub <Wilk>
2023-02-06 20:08:16 -0600

    Finished setting up alert

--- API hackwich/ContentView.swift
+++ API hackwich/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var jokes = [Joke]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             List(jokes) { joke in
@@ -22,31 +23,39 @@ struct ContentView: View {
         .onAppear() {
             getJokes()
         }
-    }
-    func getJokes() {
-        let apiKey = "?rapidapi-key=1a521986b0msh0ecfd357b9123fbp1ca6cfjsnb7ef71545a5d"
-        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
-        if let url = URL(string: query) {
-            if let data = try? Data(contentsOf: url) {
-                let json = try! JSON(data: data)
-                if json["success"] == true {
-                    let contents = json["body"].arrayValue
-                    for item in contents {
-                        let setup = item["setup"].stringValue
-                        let punchline = item["punchline"].stringValue
-                        let joke = Joke(setup: setup, punchline: punchline)
-                        jokes.append(joke)
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        }
+        }
+        func getJokes() {
+            let apiKey = "?rapidapi-key=1a521986b0msh0ecfd357b9123fbp1ca6cfjsnb7ef71545a5d"
+            let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+            if let url = URL(string: query) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = try! JSON(data: data)
+                    if json["success"] == true {
+                        let contents = json["body"].arrayValue
+                        for item in contents {
+                            let setup = item["setup"].stringValue
+                            let punchline = item["punchline"].stringValue
+                            let joke = Joke(setup: setup, punchline: punchline)
+                            jokes.append(joke)
+                        }
+                        return
                     }
                 }
+                
             }
-            
+            showingAlert = true
         }
+        struct Joke: Identifiable {
+            let id = UUID()
+            var setup = ""
+            var punchline = ""
+        }
+        
+        
     }
-    struct Joke: Identifiable {
-        let id = UUID()
-        var setup = ""
-        var punchline = ""
-    }
-    
-    
-}
+
